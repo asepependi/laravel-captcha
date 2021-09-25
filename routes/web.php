fd145bfd0e4a9f44667ca1accf5e3784c2f8f4e6
Asep Ependi <Sepp@Desktop>
2021-09-25 15:22:04 +0700

    change

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
 Route::get('/refresh_captcha', 'Auth\RegisterController@refresh')->name('refresh');
 
